5fddbf7322d57c2ef38968fbaecb84bef39dedd1
NarminGale <narmingale@gmail.com>
2021-06-24 16:21:33 +0400

    Modified return value

--- 03-ES6-Pagination/modules/displayFollowers.js
+++ 03-ES6-Pagination/modules/displayFollowers.js
@@ -4,13 +4,11 @@ const display = (followers) => {
   const newFollowers = followers
     .map((person) => {
       const { avatar_url, login, html_url } = person
-      return `
-    <article class = "card">
+      return `<article class = "card">
         <img src="${avatar_url}" alt="${login}" />
         <h4>${login}</h4>
-        <a href="${html_url}" class="btn" >view profile<a/>
-    </article>
-    `
+        <a href="${html_url}" class="btn">view profile</a>
+    </article>`
     })
     .join('')
   container.innerHTML = newFollowers
