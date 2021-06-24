b170cc10364704eef60647c424342bc313e68fa9
NarminGale <narmingale@gmail.com>
2021-06-24 16:22:42 +0400

    Imported displayButtons.js

--- 03-ES6-Pagination/app.js
+++ 03-ES6-Pagination/app.js
@@ -1,15 +1,24 @@
 import fetchFollowers from './modules/fetchFollowers.js'
 import displayFollowers from './modules/displayFollowers.js'
 import paginate from './modules/paginate.js'
+import displayButtons from './modules/displayButtons.js'
 
 const title = document.querySelector('.section-title h1')
+const btnContainer = document.querySelector('.btn-container')
+
+let index = 0
+let pages = []
+
+const setupUI = () => {
+  displayFollowers(pages[index])
+  displayButtons(btnContainer, pages, index)
+}
 
 const init = async () => {
   const followers = await fetchFollowers()
-  displayFollowers(paginate(followers)[0])
   title.textContent = 'pagination'
-  const pages = paginate(followers)
-  console.log(pages)
+  pages = paginate(followers)
+  setupUI()
 }
 
 window.addEventListener('load', init)
